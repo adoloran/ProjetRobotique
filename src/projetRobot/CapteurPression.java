3f1e00e396c0170fe3092f89f53125836df21a90
iineshinju <perlenolison@gmail.com>
2021-04-14 15:16:46 +0200

    Create CapteurPression.java

---
+++ src/projetRobot/CapteurPression.java
@@ -0,0 +1,28 @@
+package projetRobot;
+
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.sensor.EV3TouchSensor;
+
+public class CapteurPression {
+	private EV3TouchSensor CapteurPression;
+
+	public CapteurPression(String nomPort) {
+		// TODO Auto-generated constructor stub
+		CapteurPression = new EV3TouchSensor(LocalEV3.get().getPort(nomPort));
+	}
+	
+
+	 public boolean isPressed()
+	    {
+	        float[] sample = new float[1];
+	        CapteurPression.fetchSample(sample, 0);
+
+	        return sample[0] != 0;
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
