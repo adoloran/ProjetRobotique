b2df8ee75c2bf945d41eeb95dc3b1ba07843b439
iineshinju <perlenolison@gmail.com>
2021-04-14 15:17:05 +0200

    Create Jouer.java

---
+++ src/projetRobot/Jouer.java
@@ -0,0 +1,77 @@
+package projetRobot;
+
+import lejos.hardware.Button;
+
+public class Jouer {
+	private int rotation;
+	private int nbJetons;
+	private Robot robot;
+
+	public Jouer(Robot robot) {
+		// TODO Auto-generated constructor stub
+		this.robot=robot;
+		rotation = 90;
+		nbJetons = 0;
+	}
+
+	public String score() {
+		return "Vous avez " + nbJetons + " points ! ";
+	}
+
+	public void scannerZone() {
+
+	}
+
+	public void marquer() {
+		if (nbJetons == 0) {
+			
+		} else {
+
+		}
+
+	}
+
+	public void chercherPalet() {
+		if (nbJetons == 0) {
+			robot.fermer();
+			robot.ouvrir();
+			robot.avancer(30,16);
+			robot.fermer();
+			robot.tourner(15, 45);
+			robot.avancer(40, 20);
+			robot.tourner(15, -45);
+			robot.avancer(40, 20);
+			robot.ouvrir();
+			robot.avancer(15, -30);
+			robot.tourner(15, -45);
+			robot.fermer();
+		}
+	}
+
+	public void lancerJeu() {
+		System.out.println(robot.getNom()+ " va commencer à jouer ! Appuyer sur le bouton central pour commencer.");
+		// Boutton START
+		Button.ENTER.waitForPress();
+		System.out.println(score());
+
+		// Premier palet
+			chercherPalet();
+		// Autres palets
+//		while (nbJetons < 0) {
+//			// scannerZone() ;
+//					
+//		}
+
+		System.out.println("Fin de la partie");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Robot Ori = new Robot("S4", "S4","S3");
+		Robot Toby = new Robot("Toby","S2", "S4", "S3");
+		Jouer premierJeu = new Jouer(Toby);
+//		premierJeu.lancerJeu();
+		Toby.ouvrir();
+	}
+
+}
